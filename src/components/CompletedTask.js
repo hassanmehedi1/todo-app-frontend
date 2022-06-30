ec4966f24ddd9374f1a2c458e624d290945278b0
Mehedi Hassan <rehan51322@gmail.com>
2022-07-01 01:57:01 +0600

    completed tasks complete

--- src/components/CompletedTask.js
+++ src/components/CompletedTask.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useContext, useEffect, useRef, useState } from "react";
+import todoContext from "../context/todoContext";
 
 const CompletedTask = () => {
-   return (
-      <div>
-         <h2>Completed tasks</h2>
+   const context = useContext(todoContext);
+   const {todos, getCompletedTodos } = context;
+
+   useEffect(() => {
+     getCompletedTodos();
+   }, []);
+
+    const ref = useRef(null);
+    const refClose = useRef(null);
+    const [todo, setTodo] = useState({
+      id: "",
+      etitle: "",
+      etask: "",
+      eisComplete: "",
+    });
+
+    const onChange = (e) => {
+      setTodo({ ...todo, [e.target.name]: e.target.value });
+    };
+
+  return (
+    <>
+      <div className="container">
+        <div className="display-4 text-success text-center ">
+          Completed Todos
+        </div>
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Task's</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo) => {
+              return (
+                <tr key={todo._id}>
+                  <td>{todo.title}</td>
+                  <td>{todo.task}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
-   );
+    </>
+  );
 };
 
 export default CompletedTask;
