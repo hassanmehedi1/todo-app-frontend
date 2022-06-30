25cac4d22ae2ebdb9b833e6827b45d37c042ddb7
Mehedi Hassan <rehan51322@gmail.com>
2022-06-30 21:18:00 +0600

    initial com

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+   return (
+      <div>
+         <h2>Hii</h2>
+      </div>
+   );
+};
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Navbar = (props) => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-nav p-4">
+        <div className="container-fluid">
+          <div className="box">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <Link className="navbar-brand" to="/">
+            {props.title}
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  <i className="fa-solid fa-house-user"></i> Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/completedtasks"
+                >
+                  {" "}
+                  Completed Tasks
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/calender"
+                >
+                  <i className="fa-solid fa-address-card"></i> Calender
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
